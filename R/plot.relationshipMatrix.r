f986a390441855beddd7de00f027c967e23cbfd3
Valentin Wimmer <Valentin.Wimmer@kws.com>
2014-11-11 17:40:51 +0000

    version 0.10-4

--- R/plot.relationshipMatrix.r
+++ R/plot.relationshipMatrix.r
@@ -9,16 +9,35 @@ plot.relationshipMatrix <- function(x,...){
     color <- c("#FFFFFF", "#FFF7EC", "#FEE8C8", "#FDD49E", "#FDBB84", "#FDAB68", "#FC8D5A",
                "#FB7C41", "#EF6548", "#ED5031", "#D73018", "#CC0000", "#B30000", "#A52A2A",
                "#990000", "#8B2323", "#7F0000", "#660000", "#4B1B06", "#341304", "#000000")
-    Min <- min(x, na.rm=TRUE)
-    Max <- max(x, na.rm=TRUE)
-    quantiles <- round(quantile(x, probs=c(.01, .99), na.rm=TRUE), digits=1)
-    rangbreaks <- round(sum(abs(quantiles))/19*2, digits=1)*.5
-    levelbreaks <- sort(c(seq(mean(quantiles), quantiles[1], -rangbreaks), seq(mean(quantiles)+rangbreaks, quantiles[2], rangbreaks)))
-    while(length(color)-1 > length(levelbreaks)){
-      levelbreaks <- c(levelbreaks, max(levelbreaks)+rangbreaks)
+    Min <- min(relMat, na.rm=TRUE)
+    Max <- max(relMat, na.rm=TRUE)
+    if(Min == Max){
+        levelbreaks <- c(Min-.1, Max+.1)
+        color="#000000"
+    } else {
+        quantiles <- round(quantile(relMat, probs=c(.01, .99), na.rm=TRUE), digits=1)
+        if(quantiles[1] == quantiles[2]){
+            levelbreaks <- c(Min, quantiles[1], Max)
+            color <- c("#FFFFFF", "#000000")
+        } else if(quantiles[2] - quantiles[1] <.1){
+            levelbreaks <- c(Min, quantiles[1],  quantiles[2], Max)
+            color <- color[c(1,11,21)]
+        } else {
+            rangbreaks <- round(sum(abs(quantiles))/19*2, digits=1)*.5
+            if(rangbreaks == 0){
+                levelbreaks <- seq(from=quantiles[1], length.out=20, by=.05)
+            } else {
+                levelbreaks <- sort(c(seq(mean(quantiles), quantiles[1], -rangbreaks), seq(mean(quantiles)+rangbreaks, quantiles[2], rangbreaks)))
+            }
+            while(length(color)-1 > length(levelbreaks)){
+                levelbreaks <- c(levelbreaks, max(levelbreaks)+rangbreaks)
+            }
+            levelbreaks <- levelbreaks[levelbreaks < Max]
+            if(length(levelbreaks) >20) levelbreaks <- levelbreaks[1:20]
+            levelbreaks <- c(Min, levelbreaks, Max)
+            if(length(levelbreaks) <= length(color)) color <- color[c(1:(length(levelbreaks)-1), length(color))]
+        }
     }
-    levelbreaks <- c(Min, levelbreaks, Max)
-
     if (size < 35){
       levelplot(relMat,axes=FALSE,
                 col.regions=color,xlab="",ylab="",
@@ -32,6 +51,6 @@ plot.relationshipMatrix <- function(x,...){
                 scales=list(at=c(1,size/2,size),labels=c(1,"...",paste(size)),tck=0),
                 at=levelbreaks,
                 ylim=c(size+.05,0.5),
-                pretty=TRUE,...)
+                pretty=TRUE)#,...)
     }
 }
--- R/summary.relationshipMatrix.r
+++ R/summary.relationshipMatrix.r
@@ -1,16 +1,30 @@
 summary.relationshipMatrix <- function(object,...){
      relMat <- object
-     ans <- list(dim=c(nrow=nrow(relMat),ncol=ncol(relMat)),rank=qr(relMat)$rank,range.off.diagonal=c(min=min(relMat[upper.tri(relMat,diag=FALSE)]),max=max(relMat[upper.tri(relMat,diag=FALSE)])),mean.diag=mean(diag(relMat)),mean.off.diag=mean(relMat[lower.tri(relMat,diag=FALSE)]),nUnique=length(unique(relMat[upper.tri(relMat,diag=TRUE)])),diag.val=summary(diag(relMat)))
+     ans <- list(dim=c(nrow=nrow(relMat),ncol=ncol(relMat)),
+                 rank=try(qr(relMat)$rank, silent=TRUE),
+                 range.off.diagonal=c(min=min(relMat[upper.tri(relMat,diag=FALSE)], na.rm=TRUE),
+                 max=max(relMat[upper.tri(relMat,diag=FALSE)], na.rm=TRUE)),
+                 mean.diag=mean(diag(relMat), na.rm=TRUE),
+                 mean.off.diag=mean(relMat[lower.tri(relMat,diag=FALSE)], na.rm=TRUE),
+                 nUnique=length(unique(relMat[upper.tri(relMat,diag=TRUE)])),
+                 diag.val=summary(diag(relMat)),
+                 empty = sum(is.na(relMat)))
      class(ans) <- "summary.relationshipMatrix"
      ans
 }
 
 print.summary.relationshipMatrix <- function(x,...){
-    cat(" dimension                   ",x$dim[1],"x",x$dim[2],"\n")
-    cat(" rank                        ",x$rank,"\n")
-    cat(" range of off-diagonal values",x$range.off.diagonal[1],"--",x$range.off.diagonal[2],"\n")
-    cat(" mean off-diagonal values    ",x$mean.off.diag,"\n")
-    cat(" range of diagonal values    ",x$diag.val[1],"--",x$diag.val[6],"\n")
-    cat(" mean diagonal values        ",x$mean.diag,"\n")
-    cat(" number of unique values     ",x$nUnique,"\n")
+    if(class(x$rank) == "try-error") warning("\n\n  There are ", x$empty, " of ", prod(x$dim), " values missing in your relationshipMatrix!\n  ",
+                                             "Computation is done with removed 'NA' values.\n", immediate.=TRUE)
+    cat(" dimension                    ",x$dim[1],"x",x$dim[2],"\n")
+    if(class(x$rank) == "try-error"){
+        cat(" rank                          not computable because of the missing values!\n")
+    } else {
+        cat(" rank                         ",x$rank,"\n")
+    }
+    cat(" range of off-diagonal values ",x$range.off.diagonal[1],"--",x$range.off.diagonal[2],"\n")
+    cat(" mean off-diagonal values     ",x$mean.off.diag,"\n")
+    cat(" range of diagonal values     ",x$diag.val[1],"--",x$diag.val[6],"\n")
+    cat(" mean diagonal values         ",x$mean.diag,"\n")
+    cat(" number of unique values      ",x$nUnique,"\n")
 }
